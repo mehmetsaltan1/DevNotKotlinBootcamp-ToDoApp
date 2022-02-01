dcea329d7c5f74192c65558c12b244e877062aa9
Mehmet Saltan <44650542+mehmetsaltan1@users.noreply.github.com>
2022-02-01 21:16:23 +0300

    Kayit fragment data binding with new job button add project

--- app/src/main/java/com/example/todoapp/fragment/KayitFragment.kt
+++ app/src/main/java/com/example/todoapp/fragment/KayitFragment.kt
@@ -5,24 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentKayitBinding
+import com.example.todoapp.viewmodel.KayitFragmentVMF
+import com.example.todoapp.viewmodel.KayitFragmentViewModel
 
 
 class KayitFragment : Fragment() {
 
+    private lateinit var tasarim: FragmentKayitBinding
+    private lateinit var viewModel:KayitFragmentViewModel
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        tasarim = DataBindingUtil.inflate(inflater,R.layout.fragment_kayit, container, false)
+        tasarim.kayitFragment = this
+        tasarim.kayitSayfasiToolbarBaslik = "Yapılacak İş Kayıt"
+        return tasarim.root
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        val tempViewModel:KayitFragmentViewModel by viewModels(){
+            KayitFragmentVMF(requireActivity().application)
+        }
+        viewModel = tempViewModel
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_kayit, container, false)
+    fun buttonKaydetTikla(yapilacak_Ad: String) {
+        viewModel.kayit(yapilacak_Ad)
     }
-
-
 }
