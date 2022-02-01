feb022ec5bbd2ce5bbbf8fe22bc178daf2e2511b
Mehmet Saltan <44650542+mehmetsaltan1@users.noreply.github.com>
2022-02-01 17:40:03 +0300

    YapilacaklarDao and Veritabani add project

--- app/src/main/java/com/example/todoapp/entity/Yapilacaklar.kt
+++ app/src/main/java/com/example/todoapp/entity/Yapilacaklar.kt
@@ -10,5 +10,4 @@ import java.io.Serializable
 data class Yapilacaklar(@PrimaryKey(autoGenerate = true)
                         @ColumnInfo(name="yapilacak_id") @NotNull var yapilacak_id: Int,
                         @ColumnInfo(name="yapilacak_is") @NotNull var yapilacak_is: String,):Serializable{
-
                         }
---
+++ app/src/main/java/com/example/todoapp/room/YapilacaklarDao.kt
@@ -0,0 +1,22 @@
+package com.example.todoapp.room
+
+import androidx.room.*
+import com.example.todoapp.entity.Yapilacaklar
+
+@Dao
+interface YapilacaklarDao {
+    @Query("SELECT * FROM yapilacaklar")
+    suspend fun tumYapilacaklar():List<Yapilacaklar>
+
+    @Insert
+    suspend fun yapilacakEkle(yapilacak:Yapilacaklar)
+
+    @Update
+    suspend fun yapilacakGuncelle(yapilacak:Yapilacaklar)
+
+    @Delete
+    suspend fun yapilacakSil(yapilacak:Yapilacaklar)
+
+    @Query("SELECT * FROM yapilacaklar WHERE yapilacak_is like '%' || :aramaKelimesi || '%'")
+    suspend fun yapilacakAra(aramaKelimesi:String):List<Yapilacaklar>
+}
